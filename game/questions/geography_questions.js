f8d67fd4656a3aa02f0e19d3690603a1c3dffe19
unknown <nesmaalkh99@gmail.com>
2023-12-19 11:32:00 +0300

    first commit

---
+++ game/questions/geography_questions.js
@@ -0,0 +1,82 @@
+const geographyQuestions = [
+    {
+        "question": "What is the capital city of Australia?",
+        "choice1": "Sydney",
+        "choice2": "Canberra",
+        "choice3": "Melbourne",
+        "choice4": "Brisbane",
+        "answer": 2
+    },
+    {
+        "question": "Which river is the longest in Europe?",
+        "choice1": "Volga",
+        "choice2": "Danube",
+        "choice3": "Thames",
+        "choice4": "Seine",
+        "answer": 1
+    },
+    {
+        "question": "In which country would you find the ancient city of Petra?",
+        "choice1": "Greece",
+        "choice2": "Jordan",
+        "choice3": "Egypt",
+        "choice4": "Italy",
+        "answer": 2
+    },
+    {
+        "question": "What is the largest island in the world?",
+        "choice1": "Greenland",
+        "choice2": "Australia",
+        "choice3": "Borneo",
+        "choice4": "Madagascar",
+        "answer": 1
+    },
+    {
+        "question": "Which mountain range separates Europe and Asia?",
+        "choice1": "Himalayas",
+        "choice2": "Andes",
+        "choice3": "Ural Mountains",
+        "choice4": "Rocky Mountains",
+        "answer": 3
+    },
+    {
+        "question": "What is the capital of South Africa?",
+        "choice1": "Johannesburg",
+        "choice2": "Pretoria",
+        "choice3": "Cape Town",
+        "choice4": "Durban",
+        "answer": 2
+    },
+    {
+        "question": "Which ocean is the largest?",
+        "choice1": "Indian Ocean",
+        "choice2": "Atlantic Ocean",
+        "choice3": "Southern Ocean",
+        "choice4": "Pacific Ocean",
+        "answer": 4
+    },
+    {
+        "question": "In which country would you find the Great Wall?",
+        "choice1": "Japan",
+        "choice2": "China",
+        "choice3": "South Korea",
+        "choice4": "Vietnam",
+        "answer": 2
+    },
+    {
+        "question": "What is the capital city of Argentina?",
+        "choice1": "Buenos Aires",
+        "choice2": "Santiago",
+        "choice3": "Lima",
+        "choice4": "Montevideo",
+        "answer": 1
+    },
+    {
+        "question": "Which desert is known as the 'Land of the Stinging Sands'?",
+        "choice1": "Sahara Desert",
+        "choice2": "Gobi Desert",
+        "choice3": "Karakum Desert",
+        "choice4": "Namib Desert",
+        "answer": 3
+    }
+]
---
+++ game/questions/science_questions.js
@@ -0,0 +1,82 @@
+const scienceQuestions = [
+    {
+        "question": "What is the chemical symbol for water?",
+        "choice1": "H2O",
+        "choice2": "CO2",
+        "choice3": "O2",
+        "choice4": "N2",
+        "answer": 1
+    },
+    {
+        "question": "Which gas do plants absorb during photosynthesis?",
+        "choice1": "Oxygen",
+        "choice2": "Carbon Dioxide",
+        "choice3": "Nitrogen",
+        "choice4": "Methane",
+        "answer": 2
+    },
+    {
+        "question": "What is the smallest unit of matter?",
+        "choice1": "Molecule",
+        "choice2": "Atom",
+        "choice3": "Cell",
+        "choice4": "Proton",
+        "answer": 2
+    },
+    {
+        "question": "Who is known as the 'Father of Modern Physics'?",
+        "choice1": "Isaac Newton",
+        "choice2": "Albert Einstein",
+        "choice3": "Galileo Galilei",
+        "choice4": "Niels Bohr",
+        "answer": 2
+    },
+    {
+        "question": "What causes the seasons on Earth?",
+        "choice1": "Tilt of the Earth's axis",
+        "choice2": "Distance from the Sun",
+        "choice3": "Rotation speed",
+        "choice4": "Orbital shape",
+        "answer": 1
+    },
+    {
+        "question": "Which element is essential for all living organisms?",
+        "choice1": "Nitrogen",
+        "choice2": "Carbon",
+        "choice3": "Iron",
+        "choice4": "Calcium",
+        "answer": 2
+    },
+    {
+        "question": "What is the powerhouse of the cell?",
+        "choice1": "Mitochondria",
+        "choice2": "Nucleus",
+        "choice3": "Endoplasmic Reticulum",
+        "choice4": "Ribosome",
+        "answer": 1
+    },
+    {
+        "question": "Which planet is known as the 'Red Planet'?",
+        "choice1": "Venus",
+        "choice2": "Mars",
+        "choice3": "Jupiter",
+        "choice4": "Saturn",
+        "answer": 2
+    },
+    {
+        "question": "What is the speed of light?",
+        "choice1": "300,000 km/s",
+        "choice2": "150,000 km/s",
+        "choice3": "200,000 km/s",
+        "choice4": "100,000 km/s",
+        "answer": 1
+    },
+    {
+        "question": "What is the chemical symbol for gold?",
+        "choice1": "Au",
+        "choice2": "Ag",
+        "choice3": "Fe",
+        "choice4": "Cu",
+        "answer": 1
+    }
+]
